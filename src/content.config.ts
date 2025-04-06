b7fd7913efac15aadf8b245c386834afe6504d9e
ptrptrd <pptrd@protonmail.com>
2025-04-06 11:38:40 +0200

    use example blog post in dev env

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,12 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
+// Github : Clone docs repo (in Action) to ./docs/notes and load them 
+// Non CI : Load Markdown and MDX files in the `src/content/blog/`
+const blogFolderPath = import.meta.env.PROD ? './docs/notes': 'src/content/blog';
+
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './docs/notes', pattern: '**/*.{md,mdx}' }),
+	loader: glob({ base: blogFolderPath, pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
