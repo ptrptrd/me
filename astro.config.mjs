2851f95a80911da57f7d1be20117d8733e84bbbf
ptrptrd <pptrd@protonmail.com>
2025-04-06 16:30:54 +0200

    add tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ptrptrd.github.io',
 	base: "/me",
 	integrations: [mdx(), sitemap()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
