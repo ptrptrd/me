3d19f6305c6ad40c6ea377ae2e9a562305ab4249
ptrptrd <pptrd@protonmail.com>
2025-04-13 22:00:26 +0200

    add initial thought about layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ptrptrd.github.io',
-	base: "/me",
-	integrations: [mdx(), sitemap()],
-	vite: {
-		plugins: [tailwindcss()],
-	},
+    site: 'https://ptrptrd.github.io',
+    base: "/me",
+    integrations: [mdx(), sitemap(), icon()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
