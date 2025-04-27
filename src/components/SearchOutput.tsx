2b325b365854722217da74de57f587232fa77b9f
ptrptrd <pptrd@protonmail.com>
2025-04-27 17:08:55 +0200

    use header as link

--- src/components/SearchOutput.tsx
+++ src/components/SearchOutput.tsx
@@ -17,14 +17,14 @@ function SearchOutput({ results }) {
 							<li key={result.url} className="text-(--input-main-foreground) py-[5px]">
 								<div className="flex items-center border-t-3 py-[15px]">
 									<Icon icon="mdi:file-outline" fontSize="30px" className="mx-[5px]"/>
-									<h1>{result.url}</h1>
+									<h1><a href={result.url}>{result.url}</a></h1>
 								</div>
 								{ subresults.map((subresult) => {
 									return (
 										<div key={subresult.url} className="pl-[30px] mb-[10px]">
 											<div className="flex items-center">
 												<Icon icon="mdi:arrow-top-right" fontSize="20px" className="mx-[5px]"/>
-												<h2>{subresult.url}</h2>
+												<h2><a href={subresult.url}>{subresult.url}</a></h2>
 											</div>
 											<div className="content text-base" dangerouslySetInnerHTML={
 												{__html: DOMPurify.sanitize(subresult.excerpt)}
