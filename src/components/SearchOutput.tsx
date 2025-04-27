fdb5d7f9b9933b5b192d11363da251fdd4cc3257
ptrptrd <pptrd@protonmail.com>
2025-04-27 17:06:37 +0200

    add initial style for search output

--- src/components/SearchOutput.tsx
+++ src/components/SearchOutput.tsx
@@ -1,25 +1,32 @@
 import DOMPurify from 'dompurify';
+import { Icon } from "@iconify/react";
+import './searchoutput.css';
 
 function SearchOutput({ results }) {
 
 	return (
 		<div 
 			id="search-output" 
-			className="w-full max-h-[500px] bg-(--panel-main) overflow-y-auto">
+			className="w-full max-h-[500px] bg-(--input-main) overflow-y-auto p-[5px] ">
 			<ul>
 				{
 					results.map((result) => {
 						console.log(result)
 						const subresults = result.sub_results
 						return (
-							<li key={result.url} className="text-(--panel-main-foreground)">
-								<hr />
-								{result.url}
+							<li key={result.url} className="text-(--input-main-foreground) py-[5px]">
+								<div className="flex items-center border-t-3 py-[15px]">
+									<Icon icon="mdi:file-outline" fontSize="30px" className="mx-[5px]"/>
+									<h1>{result.url}</h1>
+								</div>
 								{ subresults.map((subresult) => {
 									return (
-										<div key={subresult.url}>
-											<h2>{subresult.url}</h2>
-											<div className="content" dangerouslySetInnerHTML={
+										<div key={subresult.url} className="pl-[30px] mb-[10px]">
+											<div className="flex items-center">
+												<Icon icon="mdi:arrow-top-right" fontSize="20px" className="mx-[5px]"/>
+												<h2>{subresult.url}</h2>
+											</div>
+											<div className="content text-base" dangerouslySetInnerHTML={
 												{__html: DOMPurify.sanitize(subresult.excerpt)}
 											}/>
 										</div>
